3f61b388cf6ae4e23290052a3b15bd5eb0c73e17
kana <nicht@s8.xrea.com>
2011-11-02 08:27:58 +0900

    Add front-end application skeleton

---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#columns .tweets').empty();  // Remove dummy content.
+});
+
+// __END__  {{{1
+// vim: expandtab shiftwidth=2 softtabstop=2
+// vim: foldmethod=marker
